02d668572d1344623b1091344272abcfc8d6eb49
shiva <45976367+Shivaraman21897@users.noreply.github.com>
2025-07-10 12:31:22 +0530

    Update BookingDetails.js

--- src/entity/BookingDetails.js
+++ src/entity/BookingDetails.js
@@ -12,46 +12,32 @@ module.exports = function (sequelize, DataTypes) {
       hotelDetailsId: {
         field: "hotel_details_id",
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
       },
-      name: DataTypes.STRING,
-      emailId: DataTypes.STRING,
-      phoneNumber: DataTypes.STRING,
-      paymentStatus: DataTypes.STRING,
-    },
-
-    {
-      tableName: "booking_details",
-      timestamps: false,
-    }
-  );
-  BookingDetails.associate = function (models) {
-    BookingDetails.belongsTo(models.Hotels, {
-      foreignKey: "hotelDetailsId",
-    });
-  };
-
-  return BookingDetails;
-};
-
-module.exports = function (sequelize, DataTypes) {
-  const BookingDetails = sequelize.define(
-    "BookingDetails",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      name: {
+        type: DataTypes.STRING,
       },
-      hotelDetailsId: {
-        field: "hotel_details_id",
-        type: DataTypes.INTEGER,
-        allowNull: false, // enforce that it's required if you want!
+      emailId: {
+        field: "email_id",
+        type: DataTypes.STRING,
       },
-      name: DataTypes.STRING,
-      emailId: DataTypes.STRING,
-      phoneNumber: DataTypes.STRING,
-      paymentStatus: DataTypes.STRING,
+      phoneNumber: {
+        field: "phone_number",
+        type: DataTypes.STRING,
+      },
+      paymentStatus: {
+        field: "payment_status",
+        type: DataTypes.STRING,
+      },
+      amount: {
+        type: DataTypes.FLOAT,  // total amount in INR
+        allowNull: false,
+      },
+      usdAmount: {
+        field: "usd_amount",
+        type: DataTypes.STRING, // store USD as string for accuracy
+        allowNull: false,
+      },
     },
     {
       tableName: "booking_details",
